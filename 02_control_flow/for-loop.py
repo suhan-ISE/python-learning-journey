183bb35f409e724ff811f360dcaeee3ab78bfa36
suhan khan <suhankhan.kas2729@gmail.com>
2026-08-11 14:09:36 +0530

    add for loop practice

---
+++ 02_control_flow/for-loop.py
@@ -0,0 +1,15 @@
+# A for loop in Python is used to repeat a block of code a specific number of times
+# or to step through items in a collection (like a list, string, or range).
+
+# This prints numbers from 0 to 4
+#code:
+for i in range(5):
+    print("Number:", i , end=" ")
+print()
+
+
+fruits = ["apple", "banana", "grapes"]
+
+# This loops through each item in the list
+for fruit in fruits:
+    print("I like", fruit)
