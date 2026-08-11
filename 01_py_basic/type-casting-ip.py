343f235727c7350881600ae8dfe5fbbe9986f4f1
suhan khan <suhankhan.kas2729@gmail.com>
2026-08-11 11:32:01 +0530

    add type-casting and input()

---
+++ 01_py_basic/type-casting-ip.py
@@ -0,0 +1,13 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+mark1=float(input("Enter your mark1: "))
+mark2=float(input("Enter your mark2: "))
+mark3=float(input("Enter your mark3: "))
+sum=mark1+mark2+mark3
+avg=sum/3
+print(f"hello, my name is {name} and I am {age} years old")
+print(f"your sub1 marks is {mark1}")
+print(f"your sub2 marks is {mark2}")
+print(f"your sub3 marks is {mark3}")
+print(f"your total marks for 300 is {sum}")
+print(f"your average marks is {avg}")
